6aaf9f553b79c0ee9440d02d3db22f8b7a03ac6d
vitusha-rat <smaili4eg@gmail.com>
2022-03-22 00:58:54 +0300

    done changeform component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import loginForm from "./components/loginForm/loginForm.hbs";
-import userIcon from "./components/userIcon/userIcon.hbs";
+//import loginForm from "./components/loginForm/loginForm.hbs";
+//import userIcon from "./components/userIcon/userIcon.hbs";
+import changeForm from "./components/changeForm/changeForm.hbs";
 import "./style.scss";
 
 const root = document.getElementById("root");
 // root.innerHTML = loginForm({});
-root.innerHTML = userIcon({ name: "David Jones", age: 25, location: "New York" });
+//root.innerHTML = userIcon({ name: "David Jones", age: 25, location: "New York" });
+root.innerHTML = changeForm({});
