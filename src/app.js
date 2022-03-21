0bd1590caf1824ef74565b8f22e87601ba55e57a
antdenv <antdenv@ya.ru>
2022-03-21 22:28:28 +0300

    Add new components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import loginForm from './components/loginForm/loginForm.hbs';
 import signupForm from './components/signupForm/signupForm.hbs';
+import feedPhotoCard from './components/feedPhotoCard/feedPhotoCard.hbs';
+import feedInfoCard from './components/feedInfoCard/feedInfoCard.hbs';
+import userIcon from './components/userIcon/userIcon.hbs';
 import './style.scss';
 
 const root = document.getElementById('root');
-root.insertAdjacentHTML('afterend', signupForm({}));
+root.insertAdjacentHTML('afterend', userIcon({
+    name: 'Иван Букашка',
+    age: 21,
+    location: 'Митино-Букашкино'
+}));
