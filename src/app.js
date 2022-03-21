06c9b1096cfa698ee2f4741ed44e0ee1b870f41b
vitusha-rat <smaili4eg@gmail.com>
2022-03-22 02:18:36 +0300

    added matchBlock component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 //import loginForm from "./components/loginForm/loginForm.hbs";
 //import userIcon from "./components/userIcon/userIcon.hbs";
-import changeForm from "./components/changeForm/changeForm.hbs";
+//import changeForm from "./components/changeForm/changeForm.hbs";
+import matchBlock from "./components/matchBlock/matchBlock.hbs";
 import "./style.scss";
 
 const root = document.getElementById("root");
 // root.innerHTML = loginForm({});
 //root.innerHTML = userIcon({ name: "David Jones", age: 25, location: "New York" });
-root.innerHTML = changeForm({});
+//root.innerHTML = changeForm({});
+root.innerHTML = matchBlock({});
