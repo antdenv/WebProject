d42d27214790d794b67ad3a4818c760a2002d919
vitusha-rat <smaili4eg@gmail.com>
2022-03-22 18:49:30 +0300

    comment changes

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ import "./style.scss";
 const root = document.getElementById("root");
 // root.innerHTML = loginForm({});
 //root.innerHTML = userIcon({ name: "David Jones", age: 25, location: "New York" });
-//root.innerHTML = changeForm({});
+root.innerHTML = changeForm({});
 //root.innerHTML = matchBlock({});
-root.innerHTML = messageBlock({
-    username: "Максимка",
-    message: "Очень длинное сообщение",
-    messageCount: 4,
-    imgsrc: "./static/images/avatar.png",
-});
+// root.innerHTML = messageBlock({
+//     username: "Максимка",
+//     message: "Очень длинное сообщение",
+//     messageCount: 4,
+//     imgsrc: "./static/images/avatar.png",
+// });
